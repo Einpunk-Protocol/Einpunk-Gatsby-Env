4a1a19915b2ec3f8934d10cb85942ac6231ce4b4
Einpunk Protocol <einpunkprotocol@As-MacBook-Pro.local>
2023-01-30 21:58:04 -0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Dev Einpunk Gatsby Env`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
